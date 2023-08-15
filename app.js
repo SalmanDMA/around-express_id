f9911ffc2b4e4969bc981f571c6e1af642adcf46
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-08-15 16:13:06 +0700

    refactor: :sparkles: fix name db

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // Menghubungkan ke MongoDB
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/aroundb');
 }
 main().catch((err) => console.log(err));
 
